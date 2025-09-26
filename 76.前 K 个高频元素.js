4ff0980d0c891f7f04150057c697d2ad736530db
Chris-young <3101940893@qq.com>
2025-09-26 14:31:59 +0800

    't'

--- 76.前 K 个高频元素.js
+++ 76.前 K 个高频元素.js
@@ -21,23 +21,68 @@ var topKFrequent = function (nums, k) {
     swap(i1, i2) {
       [this.heap[i1], this.heap[i2]] = [this.heap[i2], this.heap[i1]];
     }
-    getSize(){
-        return this.heap.length
+    getSize() {
+      return this.heap.length;
     }
-    getTop(){
-        return this.heap[0]
+    getTop() {
+      return this.heap[0];
     }
-    insert(freq,num){
-        this.heap.push([freq,num])
+    insert(freq, num) {
+      this.heap.push([freq, num]);
     }
 
-    extractMin(){
-        const min =this.heap[0]
-        const last=this.heap.pop()
-        if(this.heap.length>0){
-            this.heap[0]=last
-            this.shiftDown(0)
-        }
+    extractMin() {
+      const min = this.heap[0];
+      const last = this.heap.pop();
+      if (this.heap.length > 0) {
+        this.heap[0] = last;
+        this.shiftDown(0);
+      }
+      return min;
+    }
+
+    shiftUp(idx) {
+      if (idx === 0) return;
+      const parentIdx = this.getLeftIdx(idx);
+      if (this.heap[idx][0] < this.heap[parentIdx][0]) {
+        this.swap(idx, parentIdx);
+        this.shiftUp(parentIdx);
+      }
+    }
+
+    shiftDown(idx) {
+      const leftIdx = this.getLeftIdx(idx);
+      const rightIdx = leftIdx + 1;
+      let smallestIdx = idx;
+      if (
+        leftIdx < this.heap.length &&
+        this.heap[leftIdx][0] < this.heap[smallestIdx][0]
+      ) {
+        smallestIdx = leftIdx;
+      }
+      if (
+        rightIdx < this.heap.length &&
+        this.heap[rightIdx][0] < this.heap[smallestIdx][0]
+      ) {
+        smallestIdx = rightIdx;
+      }
+      if (smallestIdx !== idx) {
+        this.swap(idx, smallestIdx);
+        this.shiftDown(smallestIdx);
+      }
     }
   }
+
+  const heap = new MiniHeap();
+  for (const [num, freq] of freqMap) {
+    if (heap.getSize() < k) {
+      heap.insert(freq, num);
+    } else {
+      if (freq > heap.getTop[0]) {
+        heap.extractMin();
+        heap.insert(freq, num);
+      }
+    }
+  }
+  return heap.heap.map((item) => item[1]);
 };
