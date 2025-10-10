3e776df5b1edb0439e19c87cb112829a1993e43f
Chris-young <3101940893@qq.com>
2025-10-10 15:58:19 +0800

    ''

--- 67.搜索旋转排序数组.js
+++ 67.搜索旋转排序数组.js
@@ -4,25 +4,28 @@
  * @return {number}
  */
 var search = function (nums, target) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] === target) return mid;
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if (nums[mid] === target) return mid; // 找到目标，返回索引
 
-    if (nums[left] <= nums[mid]) {
-      if (nums[left] <= target && target < nums[mid]) {
-        right = mid - 1;
-      } else {
-        left = mid + 1;
-      }
-    } else {
-      if (nums[mid] < target && target <= nums[right]) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
+        // 判断左半部分是否有序
+        if (nums[left] <= nums[mid]) {
+            // 目标在左半有序区间内，缩小右边界
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            } else { // 目标不在左半，缩小左边界
+                left = mid + 1;
+            }
+        } else { // 右半部分有序
+            // 目标在右半有序区间内，缩小左边界
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1;
+            } else { // 目标不在右半，缩小右边界
+                right = mid - 1;
+            }
+        }
     }
-  }
-  return -1;
+    return -1; // 未找到目标
 };
--- 68.寻找旋转排序数组中的最小值.js
+++ 68.寻找旋转排序数组中的最小值.js
@@ -2,16 +2,26 @@
  * @param {number[]} nums - 旋转排序数组
  * @return {number} - 返回数组中的最小值
  */
-var findMin = function (nums) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left < right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] > nums[right]) {
-      left = mid + 1;
-    } else {
-      right = mid;
+var findMin = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    // 当 left 与 right 重合时，循环结束
+    while (left < right) {
+        const mid = Math.floor((left + right) / 2);
+
+        // 情况 1: 中点大于最右边的值
+        // 说明最小值一定在 mid 的右边
+        if (nums[mid] > nums[right]) {
+            left = mid + 1;
+        } 
+        // 情况 2: 中点小于等于最右边的值
+        // 说明最小值在 mid 左边或就是 mid
+        else {
+            right = mid;
+        }
     }
-  }
-  return nums[left];
+
+    // 最后 left 和 right 会重合，指向最小值
+    return nums[left];
 };
