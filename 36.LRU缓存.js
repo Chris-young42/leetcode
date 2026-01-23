aba80f48ece73cc55781086055be3b427fe96ac6
Your Name <you@example.com>
2026-01-23 17:49:50 +0800

    'feat'

--- 36.LRU缓存.js
+++ 36.LRU缓存.js
@@ -25,3 +25,52 @@ LRUCache.prototype.put = function (key, value) {
   }
   this.cache.set(key, value);
 }
+
+class LRUCache {
+  constructor(capacity) {
+    this.cap = capacity;
+    this.map = new Map();
+
+    // 虚拟头尾
+    this.head = {};
+    this.tail = {};
+    this.head.next = this.tail;
+    this.tail.prev = this.head;
+  }
+
+  get(key) {
+    if (!this.map.has(key)) return -1;
+    const node = this.map.get(key);
+    this._remove(node);
+    this._add(node);
+    return node.val;
+  }
+
+  put(key, value) {
+    if (this.map.has(key)) {
+      this._remove(this.map.get(key));
+    }
+    const node = { key, val: value };
+    this._add(node);
+    this.map.set(key, node);
+
+    if (this.map.size > this.cap) {
+      const lru = this.tail.prev;
+      this._remove(lru);
+      this.map.delete(lru.key);
+    }
+  }
+
+  // ===== 双向链表操作 =====
+  _remove(node) {
+    node.prev.next = node.next;
+    node.next.prev = node.prev;
+  }
+
+  _add(node) {
+    node.prev = this.head;
+    node.next = this.head.next;
+    this.head.next.prev = node;
+    this.head.next = node;
+  }
+}
